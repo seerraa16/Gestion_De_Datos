b922a009914d8cfcb91ce610134d222c104ce3df
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2025-03-06 16:40:14 +0100

    Tabla dimensional, un poco iniciado

--- dimensional.sql
+++ dimensional.sql
@@ -1,11 +1,11 @@
--- Clase
+-- Dimension Geo
 
 SELECT
 [TIENDA_ID]
     ,tienda.[PROVINCIA_ID]
     ,tienda.[ZONA_ID]
     ,[TIENDA_DESC]
-    ,[PROV_DESC]
+    ,[PROV_DESC]      
     ,[ZONA]
 
 FROM [DATAEX].[011_tienda] tienda
@@ -14,9 +14,46 @@ LEFT JOIN
 LEFT JOIN
   [DATAEX].[013_zona] zona ON tienda.ZONA_ID = zona.ZONA_ID
 
-  SELECT
-  [Id_Producto]
-    ,[CATEGORIA_ID]
+
+--- Dimension Clientes
+
+SELECT
+[Customer_ID]
+    ,clientes.[CODIGO_POSTAL]
+    ,[CP]
+    ,[CP_value]
+
+FROM [DATAEX].[003_cliente] clientes
+LEFT JOIN
+  [DATAEX].[005_cp] cp ON clientes.CODIGO_POSTAL = cp.CODIGO_POSTAL
+LEFT JOIN
+  [DATAEX].[010_Mosaic] cp_value ON clientes.CODIGO_POSTAL = cp_value.CODIGO_POSTAL
+
+
+
+--- Dimensio producto
+SELECT
+[Id_Producto]
+  ,producto.[Code_]
+  ,producto.[Fuel_ID]
+  ,producto.[CATEGORIA_ID]
+  ,producto.[Modelo]
+  ,[Fuel_ID]
+  ,[FUEL]
+  ,[CATEGORIA_ID]
+  ,[Grade_ID]
+  ,[Equipamiento]
+  ,
+
+
+--- Dimension Tiempo 
+SELECT
+[Date]
+    ,date.[Dia]
+    ,date.[Mes]
+    ,date.[Anno]
+    ,date.[Week]      
+    ,[ZONA]
     
     
 
